6a2d8aabcf82b195d935dd2dcb0361da53d18e08
matthew-mark <104662346+matthew-mark@users.noreply.github.com>
2024-01-13 19:25:58 -0500

    Update DeltaHacksX.py
    got rid of second servo, fixed flow

--- raspberrypi4/DeltaHacksX.py
+++ raspberrypi4/DeltaHacksX.py
@@ -19,7 +19,6 @@ redLED = 12
 blueLED = 19            
 greenLED = 18
 servo1 = 4
-servo2 = 7
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -27,20 +26,17 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(redLED,GPIO.OUT)    
 GPIO.setup(blueLED,GPIO.OUT)   
 GPIO.setup(greenLED,GPIO.OUT)
-GPIO.setup(servo1,GPIO.OUT) 
-GPIO.setup(servo2,GPIO.OUT)   
+GPIO.setup(servo1,GPIO.OUT)  
 
 red_pwm = GPIO.PWM(redLED,1000)
 blue_pwm = GPIO.PWM(blueLED,1000) 
 green_pwm = GPIO.PWM(greenLED,1000)
 servo1_pwm = GPIO.PWM(servo1, 50)
-servo2_pwm = GPIO.PWM(servo2, 50)
 
 red_pwm.start(0)			
 blue_pwm.start(0)                       
 green_pwm.start(0)
 servo1_pwm.start(0)
-servo2_pwm.start(0)
 
 def red_LED():
     red_pwm.ChangeDutyCycle(100)
@@ -64,11 +60,9 @@ def power_Off_LED():
     
 def open_Box():
     servo1_pwm.ChangeDutyCycle(7.5)
-    servo2_pwm.ChangeDutyCycle(7.5)
 
 def close_Box():
     servo1_pwm.ChangeDutyCycle(2.5)
-    servo2_pwm.ChangeDutyCycle(2.5)
    
 def play_sound(sound):
     pygame.mixer.init()
@@ -76,63 +70,78 @@ def play_sound(sound):
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():
         pygame.time.Clock().tick(10)
+        
+def temperature_led(temperature):
+        if temperature == -1:
+            blue_LED()
+            sleep(1)
+        elif temperature == 0:
+            green_LED()
+            sleep(1)
+        else:
+            red_LED()
+            sleep(1)
+            
+def temperature_led_with_sound(temperature):
+        if temperature == -1:
+            blue_LED()
+            sleep(1)
+            sound = dict["cold"]
+            play_sound(sound)
+        elif temperature == 0:
+            green_LED()
+            sleep(1)
+        elif temperature == 1:
+            red_LED()
+            sleep(1)
+            sound = random.choice(dict["hot"])
+            play_sound(sound)
     
 dict = {
     "cold": "ice-ice-baby.mp3",
-    #"neutral": "",
     "hot": ["ah-thats-hot.mp3"],
     "open": "fbi-open-up.mp3"
-    #"close": ""
 }
 
 door_toggle = 0
 
 try:
     while True:
+        
         db = firebase.database()
         collection_name= "box"
         temperature = db.child(collection_name).child("temperature").get().val()
         isOpen = db.child(collection_name).child("isOpen").get().val()
         isFull = db.child(collection_name).child("isFull").get().val()
         
-        if isFull == 1:
-            if isOpen == 1:
-                if door_toggle == 0:
-                    door_toggle = 1
-                    open_Box()
-                    sleep(1)
-                    sound = dict["open"]
-                    play_sound(sound)
-                        
-            elif isOpen == 0:
+        if (isFull == 1 and isOpen == 1 and door_toggle == 0):
+                door_toggle = 1
+                open_Box()
+                sleep(1)
+                temperature_led(temperature)
+                sound = dict["open"]
+                play_sound(sound)
+
+        elif (isFull == 1 and isOpen == 0):
                 close_Box()
                 sleep(1)
+                temperature_led(temperature)
                 door_toggle = 0
                 
-        elif isFull == 0:
-            if isOpen == 1:
-                if door_toggle == 0:
-                    door_toggle = 1
-                    open_Box()
-                    sleep(1)
-                    if temperature == -1:
-                        blue_LED()
-                        sound = dict["cold"]
-                        play_sound(sound)
-                    elif temperature == 0:
-                        green_LED()
-                    elif temperature == 1:
-                        red_LED()
-                        sound = random.choice(dict["hot"])
-                        play_sound(sound)
+        elif (isFull == 0 and isOpen == 1 and door_toggle == 0):
+                door_toggle = 1
+                open_Box()
+                sleep(1)
+                temperature_led_with_sound(temperature)
                         
-            elif isOpen == 0:
+        elif (isFull == 0 and isOpen == 0):
                 door_toggle = 0
                 close_Box()
                 sleep(1)
                 power_Off_LED()
             
         sleep(1)
+        
 
 except KeyboardInterrupt: 		
     pass
@@ -142,5 +151,4 @@ finally:
     blue_pwm.stop() 
     green_pwm.stop()
     servo1_pwm.stop()
-    servo2_pwm.stop()
     GPIO.cleanup()
