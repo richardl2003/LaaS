65a7993a9d6b4ccde2f10c40cf72f80c7c6ddc26
richardl2003 <richardliy03@gmail.com>
2024-01-14 00:31:02 -0500

    feat: added biometricauth to the seller flow

--- flutter/lib/pages/seller_status_page.dart
+++ flutter/lib/pages/seller_status_page.dart
@@ -2,6 +2,8 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
+import '../components/biometric_auth.dart';
+
 
 class SellerStatusPage extends StatefulWidget {
   const SellerStatusPage({super.key});
@@ -13,12 +15,35 @@ class SellerStatusPage extends StatefulWidget {
 class _SellerStatusPageState extends State<SellerStatusPage> {
   final database = FirebaseDatabase.instance.ref();
 
-  // bool hasBeenFulfilled = false;
+  bool isUserAuthenticated = false;
   List<bool> hasBeenFulfilled = List.generate(25, (index) => false);
+  void onAuthenticationComplete(bool isAuthenticated) {
+    setState(() {
+      isUserAuthenticated = isAuthenticated;
+    });
+    
+    if (isAuthenticated) {
+      openLockbox();
+    } else {
+      print('Not authenticated');
+    }
+  }
+
+  void openLockbox() async {
+    final boxRef = database.child('box/');
+    try {
+      await boxRef.update({
+        'isOpen': 1
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final buyerRef = database.child('accounts/buyers/');
+
     
     return Scaffold(
       appBar: AppBar(
@@ -43,6 +68,18 @@ class _SellerStatusPageState extends State<SellerStatusPage> {
                       setState(() {
                         hasBeenFulfilled[index] = !hasBeenFulfilled[index];
                       });
+                      
+                      if (!hasBeenFulfilled[index]) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BiometricAuth(
+                              onAuthenticationComplete: onAuthenticationComplete
+                            )
+                          )
+                        );
+                      }
+                      
                     },
                     child: Text(
                       hasBeenFulfilled[index] ? 'Open': 'Fulfill', 
